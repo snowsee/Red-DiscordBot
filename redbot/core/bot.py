4a9069e34a5b5e6b5da80f3cc21ace2b4a4c60d4
snowsee <ssnowsee@gmail.com>
2020-10-28 07:10:39 +0530

    Disable privileged intents

--- redbot/core/bot.py
+++ redbot/core/bot.py
@@ -175,7 +175,7 @@ class RedBase(
             raise RuntimeError("Red doesn't accept owner_id kwarg, use owner_ids instead.")
 
         if "intents" not in kwargs:
-            intents = discord.Intents.all()
+            intents = discord.Intents.default()
             for intent_name in cli_flags.disable_intent:
                 setattr(intents, intent_name, False)
             kwargs["intents"] = intents
